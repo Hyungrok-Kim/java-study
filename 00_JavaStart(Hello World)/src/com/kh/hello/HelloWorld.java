e13a4b20086c7702dd5331644c9847621042c229
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2020-09-25 09:23:24 +0900

    Java HelloWorld

---
+++ 00_JavaStart(Hello World)/src/com/kh/hello/HelloWorld.java
@@ -0,0 +1,52 @@
+
+package com.kh.hello;
+/**
+ *  javadoc 주석 (자바 문서 작성용 주석)
+ *  어떠한 클래스나 파일에 대한 내용을
+ *  설명하는 문서를 제작할 때 다는 주석
+ *  
+ *   @author KimHR
+ *   @when 2020-8-14
+ *   @version 1.0
+ *  
+ */
+public class HelloWorld {
+	/*
+	 * 자바 프로그램 작성 순서
+	 * - 프로젝트 생성
+	 * - 자동 생성된 src 폴더 안에 패키지 생성
+	 *   패키지(package) : 특정 목적을 가진 소스 코드들을 한 곳에 묶어 놓은 폴더 (like 양말은 양말 서랍에, 치킨은 치킨 서랍에)
+	 * 
+	 * - 클래스 파일 생성
+	 *   클래스(class) : 어떠한 요소를 작성할 때 사용하는 하나의 설계 도면 
+	 *   
+	 * - 메소드 생성 
+	 *   메소드(Method) : 특정 클래스 안에서 하나의 기능을 담담하는 함수 
+	 *   
+	 *   실행 전용 메소드 : 
+	 *		해당하는 프로젝트 전체에서 단 한 개만 존재하는 메소드 
+	 *		프로그램 실행 시 동작하는 함수(ex 세탁기의 전원 버튼) 
+	 *		public static void main(String[] args) {...}
+	 *
+	 *   기능 제공 메소드 : 
+	 *   나머지 전부 
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello world !!");
+		// TODO Auto-generated method stub
+
+		
+		double d = 1.1444;
+		System.out.println(2 * d);
+		
+	}
+
+}
+
+// : 한 줄 주석 (설명문구)
+/*
+ : 여러 줄 주석 
+ */ 
+ 
+
