2fdc18e187d90c8093d67eb2dc81ea21e52c3205
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2020-10-20 09:45:20 +0900

    JavaLogicStudy

---
+++ JavaLogicStudy/src/silsub/test/TestSilsub.java
@@ -0,0 +1,20 @@
+package silsub.test;
+
+
+import silsub.sample.*;
+
+public class TestSilsub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//new Munjae().test1();
+		//new Munjae().test2();
+		//new Munjae().test3();
+		//new Munjae().test4();
+		//new Munjae().inputTest();
+		new Munjae().test6();
+		//new Munjae().test7();
+		//new Munjae().test8();
+	}
+
+}
